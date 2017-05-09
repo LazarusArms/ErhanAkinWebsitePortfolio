f088aeedc1e0776515ec60f4042e694989e3826c
Erhan Akin <erhan@Erhans-MacBook-Pro-2.local>
2017-05-09 12:36:42 +0200

    Canvas progressie
    Canvas aangepast + objecten gecreëerd in canvas.js. Vervolgstap is het
    tekenen van deze objecten

--- docs/js/canvas.js
+++ docs/js/canvas.js
@@ -4,4 +4,11 @@ var canvas = document.querySelector('canvas');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+var c = canvas.getContext('2d');
+
+c.fillRect(200, 250, 100, 100)
+c.fillRect(100, 150, 100, 100)
+c.fillRect(400, 550, 100, 100)
+c.fillRect(800, 250, 100, 100)
+
 console.log(canvas);
